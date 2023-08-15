4dd2e13e367f2f1f61e5716a2f2e88701a232477
Moth <moth.feedback@gmail.com>
2023-08-16 00:45:15 +0300

    page404 style, page401, auth -> registration + login.
    auth page split into login and registration pages.
    
    added interception of error 401 when trying to log into someone else's account.
    
    styled 404 error page.
    
    preparations were made to connect the database and register and authorize users in the service.

--- main/main_site.py
+++ main/main_site.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, flash, abort
 import sqlite3
 import os
 from dotenv import load_dotenv
@@ -16,25 +16,46 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 
 @app.route('/')
-def index():
-    return render_template("index.html", title="Budget control - Home page")
+def homepage():
+    return render_template("homepage.html", title="Budget control - Home page")
 
 
-@app.route('/auth', methods=["GET", "POST"])  # send password in POST request
-def auth():
+@app.route('/registration', methods=["GET", "POST"])
+def registration():
     if request.method == "POST":
-        print(request.form)  # request.args - GET, request.form - POST
-    return render_template("auth.html", title="Budget control - Authorization")
+        print(request.form)
+    return render_template("registration.html", title="Budget control - Registration")
 
 
-@app.route('/household')
-def household():
-    return render_template("household.html", title="Budget control - Household")
+@app.route('/login', methods=["GET", "POST"])  # send password in POST request
+def auth():
+    if "userLogged" in session:  # If the client has logged in before
+        return redirect(url_for("household", username=session["userLogged"]))
+
+    # here the POST request is checked and the presence of the user in the database is checked
+    if request.method == "POST" and request.form.get("username") == "username":  # OK
+        session["userLogged"] = request.form["username"]
+        return redirect(url_for("household", username=session["userLogged"]))
+        # print(request.form)  # request.args - GET, request.form - POST
+
+    return render_template("login.html", title="Budget control - Login")
+
+
+@app.route('/household/<username>')  # user's personal account
+def household(username):
+    if "userLogged" not in session or session["userLogged"] != username:
+        abort(401)
+    return render_template("household.html", title=f"Budget control - {username}")
+
+
+@app.errorhandler(401)
+def page_not_found(error):
+    return render_template("error401.html", title="UNAUTHORIZED"), 401
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("error404.html", title="PAGE NOT FOUND")
+    return render_template("error404.html", title="PAGE NOT FOUND"), 404
 
 
 if __name__ == "__main__":
---
+++ main/static/js/clear_button.js
@@ -0,0 +1,7 @@
+const clearButton = document.getElementById("clear-button");
+
+clearButton.addEventListener("click", function() {
+  const input_1 = document.getElementById("input");
+
+  input_1.value = "";
+});
