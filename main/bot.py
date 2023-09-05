b400f84a477dbaebc16e793af1da1eb3f425cc6c
Moth <moth.feedback@gmail.com>
2023-09-05 13:39:22 +0300

    bot
    update bot.py

--- main/bot.py
+++ main/bot.py
@@ -2,6 +2,7 @@ import telebot
 from telebot import types
 import os
 from dotenv import load_dotenv
+from validators.registration import registration_validator, token_validator
 
 
 load_dotenv()  # Load environment variables from .env file
@@ -16,8 +17,9 @@ def start(message):
     btn1 = types.KeyboardButton("❓ Help")
     btn2 = types.KeyboardButton("🏆 Link to project github")
     btn3 = types.KeyboardButton("💻 My Telegram link")
+    btn4 = types.KeyboardButton("🤡 I want to register/login to my account")
 
-    markup.add(btn1, btn2, btn3)
+    markup.add(btn1, btn2, btn3, btn4)
 
     bot.send_message(message.chat.id, f"Hello, {message.from_user.first_name} {message.from_user.last_name}!\n"
                                       f"Are you a new user or contributor to the project?", reply_markup=markup)
@@ -42,6 +44,25 @@ def project_github(message):
 
 
 @bot.message_handler(content_types=['text'])
+def choice_login_registration(message):
+    markup_1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+
+    btn1 = types.KeyboardButton("🌚 I`m a new user")
+    btn2 = types.KeyboardButton("🌝 I`m already registered")
+
+    markup_1.add(btn1, btn2)
+
+    if message.text == "🤡 I want to register/login to my account":
+        bot.send_message(message.chat.id, "Choose the right option", reply_markup=markup_1)
+
+    if message.text == "🌚 I`m a new user":
+        login(message)
+
+    elif message.text == "🌝 I`m already registered":
+        registration(message)
+
+
+@bot.message_handler(content_types=['text'])
 def text(message):
     if message.text == "❓ Help":
         help(message)
@@ -51,6 +72,17 @@ def text(message):
 
     elif message.text == "💻 My Telegram link":
         my_link(message)
+
+    elif message.text == "🤡 I want to register/login to my account":
+        choice_login_registration(message)
+
+
+def login(message):
+    pass
+
+
+def registration(message):
+    pass
 
 
 bot.polling(none_stop=True)
