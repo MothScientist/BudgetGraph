7e91300f0bde227f2021c70adabdbb5b13bcad97
Moth <moth.feedback@gmail.com>
2023-09-16 00:19:15 +0300

    db, documentation.
    The structure of the Users and Groups tables has been changed - field uniqueness identifiers have been added. Minor code refactorings, adding documentation and comments.

--- main/create_db.sql
+++ main/create_db.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS Groups (
     id integer PRIMARY KEY AUTOINCREMENT,
-    owner text NOT NULL,
-    token text NOT NULL
+    owner text NOT NULL UNIQUE,
+    token text NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS Users (
     id integer PRIMARY KEY AUTOINCREMENT,
-    username text NOT NULL,
+    username text NOT NULL UNIQUE,
     psw_salt text NOT NULL,
     password_hash text NOT NULL,
     group_id integer NOT NULL,
-    telegram_link text NOT NULL,
+    telegram_link text NOT NULL UNIQUE,
     last_login real NOT NULL
 );
