4657716a382a3845de5d82e744f05c59b2e0b24b
Moth <moth.feedback@gmail.com>
2023-10-08 00:30:14 +0300

    minor changes

--- main/tests/registration_validate.py
+++ main/tests/registration_validate.py
@@ -1,12 +1,5 @@
 import unittest
-from main.validators.registration import registration_validator
-
-
-class RegistrationFormTest(unittest.TestCase):
-
-    def test_0(self):
-        self.assertFalse(registration_validator("", "", ""))
-
 
 if __name__ == '__main__':
-    unittest.main()
+    pass
+    # unittest.main()
