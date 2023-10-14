2d728a43150f2f0a764694680ac0b515072c9b2e
Moth <moth.feedback@gmail.com>
2023-10-15 01:58:19 +0300

    security update
    Changed hashing algorithm: sha256 -> sha3_256.
    
    The length of the resulting hash has been changed: 32 ->
    
    The length of the resulting key has been changed (get_salt): 16 -> 32.
    
    Changed the number of hashing iterations: 512 -> 1024.
    
    Added logging to track function running time.
    
    The time it takes to obtain salt for hash has increased by ~ 2 times.
    
    Hash generation time has increased by ~ 4 times.
    
    Module documentation has been changed.

--- main/password_hashing.py
+++ main/password_hashing.py
@@ -1,24 +1,38 @@
 from hashlib import pbkdf2_hmac
 from secrets import choice
 
+# Timeit decorator
+from time_checking import timeit
 
-def get_salt():
+# Warning.
+# The pseudo-random generators os.random() should not be used for security purposes.
+# For security or cryptographic uses, see the secrets module.
+
+
+@timeit
+def get_salt(key_length: int = 32) -> str:
     """
     :return: random (entropy-based) character string of length 32
     """
-    return ''.join(choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@%#&*") for _ in range(16))
+    return ''.join(choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@%#&*")
+                   for _ in range(key_length))
 
 
-def getting_hash(secure_key: str, salt: str, iterations=512, key_length=32, hash_algorithm="sha256") -> str:
+@timeit
+def getting_hash(secure_key: str, salt: str,
+                 iterations: int = 1024,
+                 key_length: int = 64,
+                 hash_algorithm: str = "sha3_256") -> str:
     """
-    :param secure_key: your password
-    :param iterations: it is best to choose a number from 500 to 2000
-    :param key_length: output key length in hex
-    :param hash_algorithm: it is best to choose sha256 or sha512 - they will provide sufficient reliability and speed
-    :param salt: Any string for unique hash generation, not currently used
+    :param secure_key: user's password
+    :param iterations: it is best to choose a number from 500 to 2000 (default = 1024)
+    :param key_length: output key length in hex (default = 64)
+    :param hash_algorithm: md5/sha512/sha256/sha512/sha3_256/sha3_512 (default = sha3_256)
+    :param salt: get_salt()
     :return: hash in hex format
     """
     return pbkdf2_hmac(hash_algorithm, secure_key.encode('utf-8'), salt.encode('utf-8'), iterations, key_length).hex()
 
 
-# print(getting_hash("34v534v53v5", ""))
+print(getting_hash("34v534v53v5", "123"))
+print(get_salt())
