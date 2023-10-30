2be6abcaa97951e35147f81507ef3cce34ecc502
Moth <moth.feedback@gmail.com>
2023-10-31 00:01:18 +0300

    issue #56

--- main/validators/correction_number.py
+++ main/validators/correction_number.py
@@ -3,19 +3,9 @@ import re
 
 def correction_number(number: str) -> int:
     """
-    Validation and transformation of a number that can be misspelled as characters within a string.
-
-    For example, 10a00 -> 1000.
-    :param number: Number as a string value.
     :return: Returns int(number) if validation passed, returns 0 (False) if validation failed.
     """
-    number: str = re.sub(r"[^0-9]", "", number)
-
     if not number or not re.match(r"^(?!0$)(?=.*\d)(?!0\d)\d{0,14}$", number):
         return 0
     else:
         return int(number)
-
-
-if __name__ == '__main__':
-    pass
