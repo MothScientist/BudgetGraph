aee5af1e7fd77b98f24be28292f6058d4518c182
Moth <moth.feedback@gmail.com>
2023-12-17 00:35:40 +0300

    refactoring webapp.py

--- app/webapp.py
+++ app/webapp.py
@@ -32,17 +32,11 @@ logger_app = setup_logger("logs/AppLog.log", "app_loger")
 
 @app.route('/')
 def homepage():
-    """
-    site's home page
-    """
     return render_template("homepage.html", title="Budget control - Home page")
 
 
 @app.route('/registration', methods=["GET", "POST"])
 def registration():
-    """
-    user registration page
-    """
     if request.method == "POST":
         username: str = request.form["username"]
         psw: str = request.form["password"]
@@ -50,25 +44,35 @@ def registration():
         token: str = request.form["token"]
 
         # If the token field is empty
-        if len(request.form['token']) == 0:  # user creates a new group
+        if len(token) == 0:  # user creates a new group
             if asyncio.run(registration_validation(username, psw, telegram_id)):
                 telegram_id: int = int(telegram_id)  # if registration_validator is passed, then it is int
                 psw_salt: str = get_salt()  # generating salt for a new user
                 dbase = DatabaseQueries(get_db())
                 user_token: str = dbase.create_new_group(telegram_id)  # we get token of the newly created group
+                group_id: int = dbase.get_group_id_by_token(user_token)
+                create_table_group(f"budget_{group_id}")
 
-                if user_token:
-                    group_id: int = dbase.get_group_id_by_token(user_token)
-                    create_table_group(f"budget_{group_id}")
-
-                    if dbase.add_user_to_db(username, psw_salt, getting_hash(psw, psw_salt), group_id, telegram_id):
-                        session.pop("userLogged", None)
-                        logger_app.info(f"Successful registration: {username}. New group created: id={group_id}.")
-                        flash("Registration completed successfully!", category="success")
-                        flash(f"{username}, your token: {user_token}", category="success_token")
+                """
+                if an error occurred while defining the variables above, 
+                then no further actions will be performed, 
+                since add_user_to_db() will return False
+                """
+                if dbase.add_user_to_db(username, psw_salt, getting_hash(psw, psw_salt), group_id, telegram_id):
+                    session.pop("userLogged", None)  # reset old cookies when registering a new user
+                    logger_app.info(f"Successful registration: {username}. New group created: id={group_id}.")
+                    flash("Registration completed successfully!", category="success")
+                    flash(f"{username}, your token: {user_token}", category="success_token")
+                else:
+                    logger_app.error(f"Error registering user with new group. Group ID: {group_id}, "
+                                     f"username: {username}, telegram ID: {telegram_id}, "
+                                     f"get_salt status: {len(psw_salt) == 32}, "
+                                     f"add_user_to_db return: {dbase.add_user_to_db(username, psw_salt, 
+                                                               getting_hash(psw, psw_salt), group_id, telegram_id)}")
+                    flash("Unknown error while creating account. Please contact technical support!", category="error")
 
         # User is added to an existing group
-        if len(token) == 32:
+        elif len(token) == 32 and token.isalnum():  # token consists only of letters and numbers
             if asyncio.run(registration_validation(username, psw, telegram_id)):
                 dbase = DatabaseQueries(get_db())
                 group_id: int = dbase.get_group_id_by_token(token)
@@ -79,7 +83,6 @@ def registration():
                     psw_salt: str = get_salt()  # generating salt for a new user
 
                     if dbase.add_user_to_db(username, psw_salt, getting_hash(psw, psw_salt), group_id, telegram_id):
-
                         flash("Registration completed successfully!", category="success")
                         logger_app.info(f"Successful registration: {username}. Group: id={group_id}.")
                     else:
@@ -95,7 +98,7 @@ def registration():
                           category="error")
 
         # User made a mistake when entering the token
-        if len(token) > 0 and len(token) != 32:
+        elif len(token) > 0 and len(token) != 32:
             logger_app.info(f"The user entered a token of incorrect length: {token}.")
             flash("Error - token length must be 32 characters", category="error")
 
@@ -104,9 +107,6 @@ def registration():
 
 @app.route('/login', methods=["GET", "POST"])  # send password in POST request and in hash
 def login():
-    """
-    user login page
-    """
     session.permanent = True
 
     if "userLogged" in session:  # If the client has logged in before
@@ -119,7 +119,7 @@ def login():
         else:
             session.pop("userLogged", None)  # removing the "userLogged" key from the session (browser cookies)
             flash("Your account was not found in the database. It may have been deleted.", category="error")
-            logger_app.warning(f"Failed registration attempt from browser cookies: {username}.")
+            logger_app.warning(f"Failed registration attempt from browser cookies: {username}")
 
     # here the POST request is checked, and the presence of the user in the database is checked
     if request.method == "POST":
@@ -135,7 +135,7 @@ def login():
             return redirect(url_for("household", username=session["userLogged"]))
         else:
             flash("This user doesn't exist.", category="error")
-        # request.args - GET, request.form - POST
+            logger_app.warning(f"Failed authorization attempt: {username}, user salt is exist: {len(psw_salt) != 0}")
 
     return render_template("login.html", title="Budget control - Login")
 
@@ -156,7 +156,6 @@ def household(username):
         if "submit-button-1" in request.form or "submit-button-2" in request.form:  # Processing "Add to table" button
             value: str = request.form.get("transfer")
             value: int = number_validation(value)
-            value: int = value
             record_date: str = request.form.get("record-date")
             record_date: str = f"{record_date[-2:]}/{record_date[5:7]}/{record_date[:4]}"  # YYYY-MM-DD -> DD/MM/YYYY
             record_date_is_valid: bool = asyncio.run(date_validation(record_date))
@@ -164,50 +163,37 @@ def household(username):
             description = request.form.get("description")
             if "submit-button-2" in request.form:
                 value: int = value * (-1)
-
-            if value:
-                if record_date_is_valid:
-                    if description_validation(description):
-                        if dbase.add_monetary_transaction_to_db(username, value, record_date, category, description):
-                            logger_app.info(f"Successfully adding data to database: "
-                                            f"operation: {request.form}"
-                                            f"table: budget_{group_id}")
-
-                            flash("Data added successfully.", category="success")
-                        else:
-                            logger_app.error(f"Error adding data to database: "
-                                             f"operation: {request.form}"
-                                             f"table: budget_{group_id}")
-
-                            flash("Error adding data to database", category="error")
-                    else:
-                        flash("Description format is invalid", category="error")
-                else:
-                    flash("Date format is invalid", category="error")
-            else:
+            if not value:
                 flash("The value format is invalid", category="error")
-
+            elif not record_date_is_valid:
+                flash("Date format is invalid", category="error")
+            elif not description_validation(description):
+                flash("Description format is invalid", category="error")
+            elif not dbase.add_monetary_transaction_to_db(username, value, record_date, category, description):
+                logger_app.error(f"Error adding data to database: "
+                                 f"operation: {request.form}"
+                                 f"table: budget_{group_id}")
+                flash("Error adding data to database", category="error")
+            else:
+                flash("Data added successfully.", category="success")
         elif "delete-record-submit-button" in request.form:
             record_id: str = request.form.get("record-id")
             record_id: int = number_validation(record_id)
 
             if not record_id or not dbase.check_id_is_exist(group_id, record_id):
                 flash("Error. The format of the entered data is incorrect.", category="error")
+            elif not dbase.delete_budget_entry_by_id(group_id, record_id):
+                logger_app.error(f"Error deletion record from database: table: budget_{group_id}, "
+                                 f"username: {username}, record id: {record_id}.")
+                flash("Error deleting a record from the database. Check that the entered data is correct.",
+                      category="error")
             else:
-                if dbase.delete_budget_entry_by_id(group_id, record_id):
-                    logger_app.info(f"Successful deletion record from database: table: budget_{group_id}, "
-                                    f"username: {username}, record id: {record_id}.")
-                    flash("Record successfully deleted", category="success")
-                else:
-                    logger_app.info(f"Error deletion record from database: table: budget_{group_id}, "
-                                    f"username: {username}, record id: {record_id}.")
-                    flash("Error deleting a record from the database. Check that the entered data is correct.",
-                          category="error")
+                flash("Record successfully deleted", category="success")
 
-    category_list = ["Supermarkets", "Restaurants", "Clothes", "Medicine", "Transport", "Devices", "Education",
-                     "Services", "Travel", "Housing", "Transfers", "Investments", "Hobby", "Jewelry", "Sale", "Salary",
-                     "Other"]
-    headers: list[str] = ["№", "Total", "Username", "Transfer", "Category", "Date", "Description"]
+    category_list: tuple[str, ...] = ("Supermarkets", "Restaurants", "Clothes", "Medicine", "Transport", "Devices",
+                                      "Education", "Services", "Travel", "Housing", "Transfers", "Investments", "Hobby",
+                                      "Jewelry", "Sale", "Salary", "Other")
+    headers: tuple[str, ...] = ("№", "Total", "Username", "Transfer", "Category", "Date", "Description")
     data: list = dbase.select_data_for_household_table(group_id, 15)  # In case of error group_id == 0 -> data = []
 
     return render_template("household.html", title=f"Budget control - {username}",
@@ -227,6 +213,7 @@ def settings(username):
     group_id: int = dbase.get_group_id_by_token(token)
     group_owner: str = dbase.get_username_group_owner_by_token(token)
     group_users_data: list = dbase.get_group_users_data(group_id)
+
     return render_template("settings.html", title=f"Settings - {username}", token=token,
                            group_owner=group_owner, group_users_data=group_users_data)
 
