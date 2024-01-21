982458dd6d705070648a725e4249284ddd25f692
Moth <moth.feedback@gmail.com>
2024-01-21 21:11:06 +0300

    The CSV file generation function has been updated, logging has been changed and detection function has been added.

--- app/csv_file_generation_and_deletion.py
+++ app/csv_file_generation_and_deletion.py
@@ -4,8 +4,7 @@ from database_control import DatabaseQueries, connect_db, close_db_main
 from log_settings import setup_logger
 from source.time_checking import timeit
 
-logger_csv = setup_logger("logs/CSVLog.log", "csv_logger")
-# change logger_csv = setup_logger("logs/SourceLog.log", "csv_logger")
+logger_csv = setup_logger("logs/SourceLog.log", "csv_logger")
 
 
 def csv_dir_check():
@@ -26,7 +25,9 @@ def create_csv_file(group_id: int):
         for record in data:
             filewriter.writerow(record)
 
-    logger_csv.info(f"Generated CSV file for group #{group_id}")
+    file_size: int | float = get_file_size_kb(f"csv_tables/table_{group_id}.csv")
+
+    logger_csv.info(f"Generated CSV file for group #{group_id} is completed. File size: {"{:.3f}".format(file_size)} kB")
 
 
 @timeit
@@ -38,3 +39,16 @@ def delete_csv_file(group_id: int):
         logger_csv.error(f"The CSV file to delete was not found. Group #{group_id}")
     except PermissionError:
         logger_csv.error(f"The CSV file to delete busy with another process. Group #{group_id}")
+
+
+def get_file_size_kb(file_path) -> int | float:
+    """
+    Returns the file size in kilobytes (float), if an error occurs, return value 0 (int)
+    """
+    try:
+        file_size: int = os.path.getsize(file_path)
+    except FileNotFoundError:
+        logger_csv.error("An error occurred while trying to determine the file size.")
+    else:
+        return file_size / 1024
+    return 0
