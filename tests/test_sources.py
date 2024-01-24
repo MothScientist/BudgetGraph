a462e277bf7947db33160def5a5883095cb908c5
GlitchTimer <156953786+GlitchTimer@users.noreply.github.com>
2024-01-24 20:04:36 +0300

    Update test_sources.py
    Adding tests for language dictionaries

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -57,18 +57,51 @@ class TestCSVGeneration(unittest.TestCase):
     pass
 
 
-class TestDictionary(unittest.TestCase):
-    def test_dictionary_1(self):
-        pass
+class TestLanguages(unittest.TestCase):
+    def test_languages_1(self):  # Checking multiple keys for presence in each language dictionary
+        _keys = ["link_github", "get_my_token", "delete_group", "services", "enter_username", "create_new_user_error",
+                 "username", "current_owner_exception", "unknown_message", "unknown_user_in_group", "delete_table"]
+        res = all(_key in Languages._languages[lang] for _key in _keys for lang in Languages._languages.keys())
+        self.assertEqual(res, True)
     
-    def test_dictionary_2(self):
-        pass
+    def test_languages_2(self):  # Checking for equality of number of keys in each language
+        dict_len = len(Languages._languages["en"])
+        res = all(dict_len == len(Languages._languages[lang]) for lang in Languages._languages.keys())
+        self.assertEqual(res, True)
     
-    def test_dictionary_3(self):
-        pass
+    def test_languages_3(self):  # Checking to see if expected values are returned
+        res = Languages.receive_translation("es", "invalid_value")
+        self.assertEqual(res, "Valor no válido")
     
-    def test_dictionary_4(self):
-        pass
+    def test_languages_4(self):
+        res = Languages.receive_translation("en", "none_token")
+        self.assertEqual(res, "(if you don't have one, enter \"None\")")
     
-    def test_dictionary_5(self):
-        pass
+    def test_languages_5(self):
+        res = Languages.receive_translation("fr", "current_owner_exception")
+        self.assertEqual(res, "C'est l'actuel propriétaire du groupe.")
+
+    def test_languages_6(self):
+        res = Languages.receive_translation("ru", "no_description")
+        self.assertEqual(res, "без описания")
+
+    def test_languages_7(self):
+        res = Languages.receive_translation("de", "change_owner")
+        self.assertEqual(res, "Besitzer wechseln")
+
+    def test_languages_8(self):
+        res = Languages.receive_translation("is", "check_correct_username")
+        self.assertEqual(res, "Athugaðu rétta stafsetningu notandanafns.")
+
+    def test_languages_9(self):  # Error exception test (language is not listed)
+        # If the language is not in the list, it will return the value in English
+        res = Languages.receive_translation("it", "view_table")
+        self.assertEqual(res, "View table")
+
+    def test_languages_10(self):  # non-existent key
+        res = Languages.receive_translation("de", "table_view")
+        self.assertEqual(res, None)
+
+    def test_languages_11(self):  # missing key and value
+        res = Languages.receive_translation("", "")
+        self.assertEqual(res, None)
