2012a5c3f9707eb0c7c08eaa38029494091313a3
Moth <moth.feedback@gmail.com>
2024-01-28 21:38:40 +0300

    changing import structures (p.6) sh script fixed

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,4 @@ cd tests || exit 1
 
 python build_test_infrastructure.py
 
-cd ..
-
 python -m pytest
--- tests/manage_test_db.py
+++ tests/manage_test_db.py
@@ -1,7 +1,9 @@
 import sqlite3
 
+db_path = "test_db.sqlite3"
 
-def connect_test_db(db_path="test_db.sqlite3"):
+
+def connect_test_db():
     try:
         conn = sqlite3.connect(db_path)
         conn.row_factory = sqlite3.Row
@@ -18,7 +20,7 @@ def close_test_db(conn):
 
 def create_test_db() -> None:
     try:
-        conn = connect_test_db(db_path="test_db.sqlite3")
+        conn = connect_test_db()
         cursor = conn.cursor()
 
         with open("create_test_db.sql", 'r') as file:
