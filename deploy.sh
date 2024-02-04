c0f853edefd702059e6e77456252b2d7f6570889
Moth <moth.feedback@gmail.com>
2024-02-05 01:16:04 +0300

    Adding a new test for GitHub Actions p.3

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ CONTAINER_NAME="budget-control-container"
 echo "Building Docker image..."
 
 # Building a Docker image based on a Dockerfile
-docker build -t $IMAGE_NAME .
+docker build --tag $IMAGE_NAME .
 
-# Checking if there is already a running container named $CONTAINER_NAME
-if [ "$(docker ps -a --filter "name=$CONTAINER_NAME" --quiet)" ]; then
+# Checks if a Docker container with a specific name "$CONTAINER_NAME" exists
+if [ "$(docker ps --all --filter "name=$CONTAINER_NAME" --quiet)" ]; then
     # If the container is already running, then stop and delete it
     docker stop $CONTAINER_NAME
     docker rm $CONTAINER_NAME
@@ -18,4 +18,6 @@ fi
 echo "Starting a container..."
 
 # Launching a new container
-docker run -d -p 5000:5000 --name $CONTAINER_NAME $IMAGE_NAME
+docker run --detach --publish 5000:5000 --name $CONTAINER_NAME $IMAGE_NAME
+
+echo "Container started"
--- run_scripts_test.sh
+++ run_scripts_test.sh
@@ -7,23 +7,35 @@
 IMAGE_NAME="budget-control-image"
 CONTAINER_NAME="budget-control-container"
 
+echo "Running the deploy.sh script..."
+
 ./deploy.sh
 
 sleep 5s
 
-if [ "$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME)" = "true" ]; then
+if [ "$(docker ps --all --filter "name=$CONTAINER_NAME" --quiet)" ]; then
+  echo "Container created: OK"
+else
+  echo "Container created: FAILED"
+fi
+
+if [ "$(docker inspect --filter '{{.State.Running}}' $CONTAINER_NAME)" = "true" ]; then
+  echo "Container started: OK"
   docker stop $CONTAINER_NAME
 else
+  echo "Container started: FAILED"
   exit 1
 fi
 
 sleep 5s
 
-if [ "$(docker inspect -f '{{.State.Running}}' $CONTAINER_NAME)" = "false" ]; then
+if [ "$(docker inspect --filter '{{.State.Running}}' $CONTAINER_NAME)" = "false" ]; then
+  echo "Container stopped: OK"
+  echo "Removing a container and image from the system..."
   docker rm $CONTAINER_NAME
   docker image rm $IMAGE_NAME
-
   exit 0
 else
+  echo "Container stopped: FAILED"
   exit 1
 fi
