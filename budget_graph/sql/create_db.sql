5d44450100c8adea5feedf0c69e55421331f30fd
Moth <moth.feedback@gmail.com>
2024-05-19 20:24:45 +0300

    hotfix #2

--- budget_graph/sql/create_db.sql
+++ budget_graph/sql/create_db.sql
@@ -3,7 +3,7 @@ CREATE SCHEMA IF NOT EXISTS "budget_graph";
 
 -- Creating tables:
 CREATE TABLE IF NOT EXISTS "budget_graph"."groups" (
-    "id"    smallserial        PRIMARY KEY, -- It makes sense to add a UNIQUE or PRIMARY KEY constraint on this
+    "id"           smallserial PRIMARY KEY, -- It makes sense to add a UNIQUE or PRIMARY KEY constraint on this
                                             -- column to protect against erroneously adding duplicate values,
                                             -- but this does not happen automatically
     "owner"        bigint      NOT NULL UNIQUE CHECK("owner" BETWEEN 1 AND POWER(10, 12) - 1),
@@ -16,17 +16,17 @@ CREATE TABLE IF NOT EXISTS "budget_graph"."users" (
     "username"    varchar(20) NOT NULL    UNIQUE CHECK(LENGTH("username") BETWEEN 3 AND 20),
     "psw_salt"    varchar(32) NOT NULL           CHECK(LENGTH("psw_salt") = 32),
     "psw_hash"    varchar(64) NOT NULL           CHECK(LENGTH("psw_hash") = 64),
-    "group_id"    integer     NOT NULL           CHECK("group_id" > 0), -- индекс по группе
+    "group_id"    integer     NOT NULL           CHECK("group_id" > 0),
     "last_login"  varchar(19)     NULL  -- переделать на тип date (как в monetary_transactions в поле record_date)
 );
 CREATE TABLE IF NOT EXISTS "budget_graph"."monetary_transactions" (
-    "group_id"       smallint    NOT NULL CHECK("group_id" > 0), -- индекс (отдельный)
+    "group_id"       smallint    NOT NULL CHECK("group_id" > 0),
     "transaction_id" integer     NOT NULL CHECK("transaction_id" > 0),
     "username"       varchar(20) NOT NULL CHECK(LENGTH("username") BETWEEN 3 AND 20),
     "total"          integer     NOT NULL,
     "transfer"       integer     NOT NULL  CHECK("transfer" <> 0),
-    "record_date"    date        NOT NULL, -- (group_id, record_date) индекс, чтобы брать аналитику по срезам дат
-    "category"       varchar(25)     NULL, -- индекс (group_id, category) + (username, category) + (record_date, category) - пригодится для аналитики -> добавить NULL в БЛ
+    "record_date"    date        NOT NULL,
+    "category"       varchar(25)     NULL, -- -> добавить NULL в БЛ
     "description"    varchar(50)     NULL, -- -> добавить NULL в БЛ
     PRIMARY KEY ("group_id", "transaction_id")
 );
@@ -36,8 +36,8 @@ CREATE TABLE IF NOT EXISTS "budget_graph"."user_languages_telegram" (
     PRIMARY KEY    ("telegram_id")
 );
 CREATE TABLE IF NOT EXISTS "budget_graph"."premium_users" (
-    "telegram_id" bigint  NOT NULL CHECK("telegram_id" BETWEEN 1 AND POWER(10, 12) - 1),
-    "group_id"    integer NOT NULL CHECK("group_id" > 0),
+    "telegram_id"    bigint  NOT NULL CHECK("telegram_id" BETWEEN 1 AND POWER(10, 12) - 1),
+    "paid_until"     date    NOT NULL,
+    "premium_status" boolean NOT NULL DEFAULT False, -- автоматическое заполнение в зависимости от "paid_until"
     PRIMARY KEY   ("telegram_id")
-    -- FOREIGN KEY (group_id) REFERENCES "premium_groups"."id"
 );
--- budget_graph/sql/indexes.sql
+++ budget_graph/sql/indexes.sql
@@ -1,14 +1,24 @@
--- Automatic indexes:
--- CREATE UNIQUE INDEX groups_pkey ON budget_graph.groups USING btree (id)
--- CREATE UNIQUE INDEX groups_owner_key ON budget_graph.groups USING btree (owner)
--- CREATE UNIQUE INDEX groups_token_key ON budget_graph.groups USING btree (token)
--- CREATE UNIQUE INDEX monetary_transactions_pkey ON budget_graph.monetary_transactions USING btree (group_id, transaction_id)
--- CREATE UNIQUE INDEX users_pkey ON budget_graph.users USING btree (telegram_id)
--- CREATE UNIQUE INDEX users_username_key ON budget_graph.users USING btree (username)
+-- Indexes defined when tables are created:
+-- CREATE UNIQUE INDEX groups_pkey                  ON budget_graph.groups                  USING btree (id)
+-- CREATE UNIQUE INDEX groups_owner_key             ON budget_graph.groups                  USING btree (owner)
+-- CREATE UNIQUE INDEX groups_token_key             ON budget_graph.groups                  USING btree (token)
+-- CREATE UNIQUE INDEX monetary_transactions_pkey   ON budget_graph.monetary_transactions   USING btree (group_id, transaction_id)
+-- CREATE UNIQUE INDEX users_pkey                   ON budget_graph.users                   USING btree (telegram_id)
+-- CREATE UNIQUE INDEX users_username_key           ON budget_graph.users                   USING btree (username)
 -- CREATE UNIQUE INDEX user_languages_telegram_pkey ON budget_graph.user_languages_telegram USING btree (telegram_id)
 
--- Creating Indexes:
-CREATE         INDEX  IF NOT EXISTS  "users_group_id"                                 ON "budget_graph"."users"                 USING btree  ("group_id");
-CREATE UNIQUE  INDEX  IF NOT EXISTS  "users_lower_username"                           ON "budget_graph"."users"                 USING btree  (LOWER("username"));
-CREATE         INDEX  IF NOT EXISTS  "monetary_transactions_group_id"                 ON "budget_graph"."monetary_transactions" USING btree  ("group_id");
-CREATE UNIQUE  INDEX  IF NOT EXISTS  "monetary_transactions_group_id_transaction_id"  ON "budget_graph"."monetary_transactions" USING btree  ("group_id", "transaction_id" DESC);
+-- Creating Indexes
+-- to get a list of group users:
+CREATE        INDEX IF NOT EXISTS "users_group_id"                       ON "budget_graph"."users"                 USING btree ("group_id");
+-- index to check the uniqueness of the username:
+CREATE UNIQUE INDEX IF NOT EXISTS "users_lower_username"                 ON "budget_graph"."users"                 USING btree (LOWER("username"));
+-- to get a list of group transactions:
+CREATE        INDEX IF NOT EXISTS "transactions_group_id"                ON "budget_graph"."monetary_transactions" USING btree ("group_id");
+-- index to search for a specific record:
+CREATE UNIQUE INDEX IF NOT EXISTS "transactions_transaction_id_group_id" ON "budget_graph"."monetary_transactions" USING btree ("transaction_id" DESC, "group_id");
+
+-- Indexes for analytics
+-- to collect group statistics for a certain period:
+CREATE INDEX IF NOT EXISTS "transactions_group_id_record_data" ON "budget_graph"."monetary_transactions" USING btree ("group_id", "record_data");
+-- to collect user statistics for a certain period:
+CREATE INDEX IF NOT EXISTS "transactions_username_record_data" ON "budget_graph"."monetary_transactions" USING btree ("username", "record_data");
