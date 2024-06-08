b94d3a9e7aa3b7a0d507b4c718a819a8d0fcaf8e
Moth <moth.feedback@gmail.com>
2024-06-08 23:33:14 +0300

    improved deletion of tables and SQL functions and triggers

--- budget_graph/build_project.py
+++ budget_graph/build_project.py
@@ -8,12 +8,72 @@ def drop_tables_in_db() -> None:
     conn = connect_db()
     try:
         with conn.cursor() as cur:
-            cur.execute("""DROP TABLE IF EXISTS "budget_graph"."groups";
-                           DROP TABLE IF EXISTS "budget_graph"."users";
-                           DROP TABLE IF EXISTS "budget_graph"."monetary_transactions";
-                           DROP TABLE IF EXISTS "budget_graph"."user_languages_telegram";
-                           DROP TABLE IF EXISTS "budget_graph"."premium_users";
-                           DROP TABLE IF EXISTS "budget_graph"."premium_groups"
+            cur.execute("""
+                           DO $$ DECLARE
+                             r RECORD;
+                           BEGIN
+                             FOR r IN (
+                                       SELECT 
+                                         tablename 
+                                       FROM 
+                                         pg_tables 
+                                       WHERE 
+                                         schemaname = 'budget_graph'
+                                       ) LOOP
+                               EXECUTE 
+                               'DROP TABLE 
+                               "budget_graph".' 
+                               || 
+                               quote_ident(r.tablename) 
+                               || 
+                               ' CASCADE';
+                             END LOOP;
+                           END $$;
+                            
+                           DO $$ DECLARE
+                             r RECORD;
+                           BEGIN
+                             FOR r IN (
+                                       SELECT 
+                                         routine_name 
+                                       FROM 
+                                         information_schema.routines 
+                                       WHERE 
+                                         specific_schema = 'budget_graph'
+                                       ) LOOP
+                               EXECUTE 
+                               'DROP FUNCTION 
+                               "budget_graph".' 
+                               || 
+                               quote_ident(r.routine_name) 
+                               || 
+                               '() CASCADE';
+                             END LOOP;
+                           END $$;
+                           
+                           DO $$ DECLARE
+                             r RECORD;
+                           BEGIN
+                             FOR r IN (
+                                       SELECT 
+                                         trigger_name, event_object_table 
+                                       FROM 
+                                         information_schema.triggers 
+                                       WHERE 
+                                         trigger_schema = 'budget_graph'
+                                       ) LOOP
+                               EXECUTE 
+                                 'DROP TRIGGER ' 
+                                 || 
+                                 quote_ident(r.trigger_name) 
+                                 || 
+                                 ' ON "budget_graph".' 
+                                 || 
+                                 quote_ident(r.event_object_table) 
+                                 || 
+                                 ' CASCADE';
+                             END LOOP;
+                           END $$;
                         """)
             conn.commit()
     except Exception as err:
@@ -59,5 +119,6 @@ def create_tables_in_db() -> None:
 
 
 if __name__ == '__main__':
+    drop_tables_in_db()
     create_directories()
     create_tables_in_db()
