44ef6bb61eb54779fe922a781b713cd56ee0dd74
Moth <moth.feedback@gmail.com>
2024-06-16 21:29:25 +0300

    moved a large SQL query into a separate file

--- budget_graph/build_project.py
+++ budget_graph/build_project.py
@@ -8,73 +8,8 @@ def drop_tables_in_db() -> None:
     conn = connect_db()
     try:
         with conn.cursor() as cur:
-            cur.execute("""
-                           DO $$ DECLARE
-                             r RECORD;
-                           BEGIN
-                             FOR r IN (
-                                       SELECT 
-                                         tablename 
-                                       FROM 
-                                         pg_tables 
-                                       WHERE 
-                                         schemaname = 'budget_graph'
-                                       ) LOOP
-                               EXECUTE 
-                               'DROP TABLE 
-                               "budget_graph".' 
-                               || 
-                               quote_ident(r.tablename) 
-                               || 
-                               ' CASCADE';
-                             END LOOP;
-                           END $$;
-                            
-                           DO $$ DECLARE
-                             r RECORD;
-                           BEGIN
-                             FOR r IN (
-                                       SELECT 
-                                         routine_name 
-                                       FROM 
-                                         information_schema.routines 
-                                       WHERE 
-                                         specific_schema = 'budget_graph'
-                                       ) LOOP
-                               EXECUTE 
-                               'DROP FUNCTION 
-                               "budget_graph".' 
-                               || 
-                               quote_ident(r.routine_name) 
-                               || 
-                               '() CASCADE';
-                             END LOOP;
-                           END $$;
-                           
-                           DO $$ DECLARE
-                             r RECORD;
-                           BEGIN
-                             FOR r IN (
-                                       SELECT 
-                                         trigger_name, event_object_table 
-                                       FROM 
-                                         information_schema.triggers 
-                                       WHERE 
-                                         trigger_schema = 'budget_graph'
-                                       ) LOOP
-                               EXECUTE 
-                                 'DROP TRIGGER ' 
-                                 || 
-                                 quote_ident(r.trigger_name) 
-                                 || 
-                                 ' ON "budget_graph".' 
-                                 || 
-                                 quote_ident(r.event_object_table) 
-                                 || 
-                                 ' CASCADE';
-                             END LOOP;
-                           END $$;
-                        """)
+            with open(path.join(path.dirname(__file__), 'sql/db_cleanup.sql'), 'r', encoding='utf-8') as sql_file:
+                cur.execute(sql_file.read())
             conn.commit()
     except Exception as err:
         print(f'Critical error when deleting tables in the database: {err}')
