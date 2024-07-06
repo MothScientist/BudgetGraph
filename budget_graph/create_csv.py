e83a890f8ddc667727eeae6fdf5176398632259e
Moth <moth.feedback@gmail.com>
2024-07-06 19:46:35 +0300

    updating the csv file generation module

--- budget_graph/create_csv.py
+++ budget_graph/create_csv.py
@@ -2,19 +2,31 @@ from os import path as os_path
 from csv import writer as csv_writer, QUOTE_MINIMAL
 from hashlib import sha256
 
+from budget_graph.dictionary import receive_translation
+
 
 class CsvFileWithTable:
     """
     Class for working with *.csv files: creating, updating, obtaining the file hash, file size and validating user data
     """
-
     def __init__(self, file_path: str,
                  table_data: tuple[[tuple, ...], ...],
                  table_headers: tuple[str, ...] =
-                 ('ID', 'USERNAME', 'TRANSFER', 'TOTAL', 'DATE', 'CATEGORY', 'DESCRIPTION')):
+                 ('ID', 'USERNAME', 'TRANSFER', 'TOTAL', 'DATE', 'CATEGORY', 'DESCRIPTION'),
+                 lang: str = 'en'):
         self.file_path: str = file_path
-        self.table_data: tuple[tuple[int, str, int, int, str, str, str], ...] = table_data
+        self.table_data: tuple[tuple, ...] = table_data
         self.table_headers: tuple[str, ...] = table_headers
+        self.language: str = lang
+        # translate if it matches the standard set of headers
+        if table_data == ('ID', 'USERNAME', 'TRANSFER', 'TOTAL', 'DATE', 'CATEGORY', 'DESCRIPTION') and lang != 'en':
+            self.table_data = (receive_translation(lang, 'ID'),
+                               receive_translation(lang, 'USERNAME'),
+                               receive_translation(lang, 'TRANSFER'),
+                               receive_translation(lang, 'TOTAL'),
+                               receive_translation(lang, 'DATE'),
+                               receive_translation(lang, 'CATEGORY'),
+                               receive_translation(lang, 'DESCRIPTION'))
 
     def create_csv_file(self):
         """
@@ -36,25 +48,19 @@ class CsvFileWithTable:
         return file_size / 1024
 
     def get_file_checksum(self) -> str:
-        hash_sha256 = sha256()  # noqa
-        with open(self.file_path, 'rb', encoding='utf-8') as csv_file:
+        hash_sha256 = sha256()
+        with open(self.file_path, 'rb') as csv_file:
             for chunk in iter(lambda: csv_file.read(1024), b""):
                 hash_sha256.update(chunk)
         return hash_sha256.hexdigest()
 
-    # TODO - а что если переделать это на дескриптор?
     def validate_incoming_data(self):
         """
         Checking the input data so that the application does not crash due to a *.csv generation error
         """
-        # TODO - проверка на валидность директории -> мб есть функция готовая или тестовый ping сделать с отловом ошибки
         # table_data is never empty when calling a function
-        if not self.table_headers:
-            raise ValueError('table_headers values cannot be empty')
-        # TODO - сделать проверку на отсутствие None и '' для всех вложенных кортежей
-        if not self.table_data or not all(element is not None and element != '' for element in self.table_data):
-            # not all(map(len, table_data)) -> no nested tuple is empty
-            raise ValueError('table_data parameters cannot be empty')
+        if not self.table_headers or not all(bool(header) for header in self.table_headers):
+            raise ValueError('Table headers values cannot be empty')
+        # the length of each nested tuple is equal to the number of columns in the table
         if not all(len(self.table_headers) == len(_row) for _row in self.table_data):
-            # the length of each nested tuple is equal to the number of columns in the table
             raise ValueError('Data tuples have different lengths and/or their lengths do not match the headers')
