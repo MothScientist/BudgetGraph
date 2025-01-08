60e5a60af18019511768be0fb08639b0c1bdad89
Moth <moth.feedback@gmail.com>
2025-01-08 19:33:24 +0300

    add check global config for timeit decorator

--- budget_graph/time_checking.py
+++ budget_graph/time_checking.py
@@ -3,16 +3,23 @@ from time import perf_counter
 from functools import wraps
 path.append('../')
 from budget_graph.logger import setup_logger
+from budget_graph.global_config import GlobalConfig
 
 logger_time = setup_logger("logs/TimeLog.log", "time_logger")
+timeit_enable: bool = GlobalConfig.timeit_enable  # create a link to the var for convenience
 
 
 def timeit(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
+
+        if not timeit_enable:
+            return func(*args, **kwargs)
+
         _start = perf_counter()
         result = func(*args, **kwargs)
         _end = perf_counter()
         logger_time.info(f'{func.__name__}: {_end - _start:.6f} s.')
         return result
+
     return wrapper
