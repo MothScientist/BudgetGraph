9eb366e00f037aabfcfe43ec767ef091fdf720b8
Moth <moth.feedback@gmail.com>
2025-01-09 23:27:47 +0300

    test localization disable init and .yaml files

---
+++ tests/test_localization_disable.py
@@ -0,0 +1,15 @@
+"""
+Checking some unit tests with a modified .toml config when deploying an application
+"""
+
+import unittest
+
+
+class TestConfigLanguages(unittest.TestCase):
+    """
+    Testing application localization modules when the parameter in the .toml file localization_enable = false
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_unit_with_modified_toml_config.py
+++
@@ -1,8 +0,0 @@
-"""
-Checking some unit tests with a modified .toml config when deploying an application
-"""
-
-import unittest
-
-if __name__ == '__main__':
-    unittest.main()
